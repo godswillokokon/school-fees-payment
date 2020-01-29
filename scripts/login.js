ae2a98b5ec275c6e1cfb90af90d8a410e2a9347e
godswillokokon <godswillokokon3@gmail.com>
2020-01-29 11:25:19 +0100

    login done

---
+++ scripts/login.js
@@ -0,0 +1,62 @@
+auth.onAuthStateChanged(user => {
+  if (user) {
+    console.log("user logged in");
+  } else {
+    console.log("user not logged in");
+  }
+});
+//login
+const loginForm = document.querySelector("#login-form");
+loginForm.addEventListener("submit", e => {
+  e.preventDefault();
+  //get user info
+  const email = loginForm["email_2"].value;
+  const password = loginForm["password_2"].value;
+
+  sessionStorage.setItem(
+    "email",
+    email
+  );
+
+  auth
+    .signInWithEmailAndPassword(email, password)
+    .then(cred => {
+      firebase
+        .auth()
+        .currentUser.getIdToken(true)
+        .then(function (idToken) {
+          // Send token to your backend via HTTPS
+          let currentUsers = firebase.auth().currentUser;
+          console.log(currentUsers, "user");
+
+          console.log(idToken, "token");
+          console.log(currentUser, "session");
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+      let currentUser = sessionStorage.setItem(
+        "user",
+        firebase.auth().currentUser
+      );
+
+      setTimeout(function () {
+        location.href = "index.html";
+      }, 3000);
+
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+function logout() {
+  //  e.preventDefault();
+  // clearCookie();
+  auth.signOut().then(() => {
+    let currentUser = sessionStorage.removeItem("user");
+    console.log("user is logging out");
+    // location.href = "index.html";
+  });
+  // console.log("am logging out");
+}
